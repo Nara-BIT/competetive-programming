0f8ec0c2778a0b4704b3eba1f57b6239689d7fd2
Nara-BIT <narasinghjadhav03@gmail.com>
2026-06-16 16:55:43 +0530

    Longest Substring without repeating characters

---
+++ two-pointers/3LC.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        int l=0;
+        int r=0;
+        map<int,int>freq;
+        int ans=0;
+        while(r<n){
+            char c=s[r];
+            freq[c]++;
+            char ch;
+            while(freq[c]>1 && l<r){
+                ch=s[l];
+                freq[ch]--;
+                l++;
+                
+            }
+            
+            ans=max(ans,r-l+1);
+
+            r++;
+        }
+        return ans;
+
+    }
