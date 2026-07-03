3220afc3a2f9e378fc5931bc8510ac19ddc144d5
Narasingh S J <narasinghjadhav03@gmail.com>
2026-07-03 11:59:06 +0530

    Time: 4 ms (33.21%), Space: 10.4 MB (41.89%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<string>>res;
+    string row="";
+    vector<string>board;
+    bool isSafe(int row,int col,int n){
+        int r=row;
+        int c=col;
+        while(0<=r && r<n && 0<=c && c<n){
+            if(board[r][c]=='Q'){
+                return false;
+            }
+            r--;
+            c--;
+        }
+        r=row;
+        c=col;
+        while(0<=r && r<n && 0<=c && c<n){
+            if(board[r][c]=='Q')
+                return false;
+            r--;
+            c++;
+        }
+        r=row;
+        c=col;
+        while(0<=r && r<n && 0<=c && c<n){
+            if(board[r][c]=='Q')
+                return false;
+            r--;
+        }
+        r=row;
+        c=col;
+        while(0<=r && r<n && 0<=c && c<n){
+            if(board[r][c]=='Q')
+                return false;
+            c--;
+        }
+        return true;
+        
+    }
+    void backtrack(int ind,int n){
+        if(ind==n){
+            res.push_back(board);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(isSafe(ind,i,n)){
+                board[ind][i]='Q';
+                backtrack(ind+1,n);
+                board[ind][i]='.';
+            }
+        }
+        return;
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        board.assign(n,string(n,'.'));
+        backtrack(0,n);
+        return res;
+    }
+};
