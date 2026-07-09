0480bcd292ca4a780e2603e144ad881cc99fc439
Narasingh S J <narasinghjadhav03@gmail.com>
2026-07-09 20:07:15 +0530

    Time: 0 ms (100%), Space: 20.6 MB (60.44%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int curr_end=0;
+        int n=nums.size();
+        int jump=0;
+        int farthest=0;
+        for(int i=0;i<n-1;i++){
+            farthest=max(farthest,i+nums[i]);
+            if(i==curr_end){
+                jump++;
+                curr_end=farthest;
+            }
+        }
+
+        return jump;
+    }
+};
