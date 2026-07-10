5b5b243e79a9a882f955ca3be7362774a410bb3c
Narasingh S J <narasinghjadhav03@gmail.com>
2026-07-10 09:15:33 +0530

    Time: 20 ms (4.31%), Space: 28 MB (45.91%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -18,11 +18,11 @@ public:
         int left=max(Traverse(root->left,ans),0);
         int right=max(Traverse(root->right,ans),0);
         ans=max(ans,root->val+left+right);
-        return root->val+left+right;
+        return root->val+max(left,right);
 
     }
     int maxPathSum(TreeNode* root) {
-        int ans=INT_MIN;
+        int ans=root->val;
         int k=Traverse(root,ans);
         return ans;
     }
