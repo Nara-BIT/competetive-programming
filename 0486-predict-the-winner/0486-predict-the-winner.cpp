29a9ff4df2a307ed21d849f4a61d96d3fad22054
Narasingh S J <narasinghjadhav03@gmail.com>
2026-08-01 10:00:24 +0530

    Time: 91 ms (22.18%), Space: 10.2 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int backtrack(int left, int right,vector<int>& nums,vector<vector<int>>&dp){
+    /*int backtrack(int left, int right,vector<int>& nums,vector<vector<int>>&dp){
         if(left>right){
             return 0;
         }
@@ -10,14 +10,24 @@ public:
         int lp=nums[left]+min(backtrack(left+2,right,nums,dp),backtrack(left+1,right-1,nums,dp));
         int rp=nums[right]+min(backtrack(left,right-2,nums,dp),backtrack(left+1,right-1,nums,dp));
         return dp[left][right]=max(lp,rp);
+    }*/
+    int pw(int i,int j,vector<int>&nums){
+        if(i==j){
+            return nums[i];
+
+        }
+        int lp=nums[i]-pw(i+1,j,nums);
+        int rp=nums[j]-pw(i,j-1,nums);
+        return max(lp,rp);
     }
     bool predictTheWinner(vector<int>& nums) {
         int t_sum=accumulate(nums.begin(),nums.end(),0);
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        int lk=backtrack(0,nums.size()-1,nums,dp);
-        if(2*lk>=t_sum)
-            return true;
+        //int lk=backtrack(0,nums.size()-1,nums,dp);
+        return pw(0,nums.size()-1,nums)>=0;
+        //if(2*lk>=t_sum)
+            //return true;
         return false;
     }
 };
