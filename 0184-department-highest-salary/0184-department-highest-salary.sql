01d7e5b8019e50813cdfd3235eeaefb8bfa65d83
Narasingh S J <narasinghjadhav03@gmail.com>
2026-08-08 19:08:30 +0530

    Time: 1562 ms (5%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-SELECT D.name AS Department, E.name AS Employee, E.salary as Salary FROM Employee E LEFT JOIN Department D 
-ON E.departmentId=D.id WHERE (E.departmentId, E.salary) 
-IN (SELECT departmentId,MAX(salary) FROM Employee GROUP BY departmentId);
+SELECT Department,Employee,Salary FROM
+(SELECT D.name as Department, E.name as Employee, E.salary AS Salary,
+    DENSE_RANK() OVER(PARTITION BY E.departmentId ORDER BY E.salary DESC) AS pr
+FROM Employee E LEFT JOIN Department D ON E.departmentId=D.id) AS P
+WHERE pr=1;
