062492d19c23ba9b04cd7133746aa552054aa237
Narasingh S J <narasinghjadhav03@gmail.com>
2026-08-27 11:51:29 +0530

    Time: 450 ms (40.71%), Space: 372.5 MB (21.43%) - LeetHub

--- 3409-longest-subsequence-with-decreasing-adjacent-difference/3409-longest-subsequence-with-decreasing-adjacent-difference.cpp
+++ 3409-longest-subsequence-with-decreasing-adjacent-difference/3409-longest-subsequence-with-decreasing-adjacent-difference.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     
-    
     int longestSubsequence(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(301,vector<int>(301,0));
+        vector<vector<int>>dp(303,vector<int>(303,0));
         vector<vector<int>>suffix(301,vector<int>(301,0));
         int ans=0;
         for(int num:nums){
-            if(dp[num][0]<1){
-                dp[num][0]=1;
-            }
-            for(int prev=1;prev<=300;prev++){
-                int dnew=abs(num-prev);
-                int len=suffix[prev][dnew];
-                if(len>0){
-                    dp[num][dnew]=max(dp[num][dnew],1+len);
-
+            int mx=0;
+            for(int j=300;j>=0;j--){
+                int ct=0;
+                int prevL=num-j;
+                int prevG=num+j;
+                if(prevL>=0){
+                    int l=1+dp[prevL][j];
+                    ct=max(l,ct);
                 }
-                else{
-                    if(suffix[prev][0]>0)
-                        dp[num][dnew]=max(dp[num][dnew],2);
+                if(prevG<=300){
+                    int l=1+dp[prevG][j];
+                    ct=max(l,ct);
                 }
-                ans=max(ans,dp[num][dnew]);
-            }
-            suffix[num][299]=dp[num][299];
-            for(int i=299;i>=0;i--){
-                suffix[num][i]=max(dp[num][i],suffix[num][i+1]);
+                mx=max(mx,ct);
+                dp[num][j]=mx;
+                ans=max(mx,ans);
             }
         }
         return ans;
